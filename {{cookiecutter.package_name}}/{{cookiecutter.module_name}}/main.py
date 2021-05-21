35555b03d9693ac5bbcd9f2620c6c411160efac2
Ingo Weinmann <mail@ingoweinmann.de>
2021-05-21 13:06:40 +0200

    Fix typer app and add verbosity callback

--- {{cookiecutter.package_name}}/{{cookiecutter.module_name}}/main.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.module_name}}/main.py
@@ -1,21 +1,32 @@
+import logging
+
 import typer
 
+logger = logging.getLogger(__name__)
+
 typer_app = typer.Typer()
+state = {"verbose": False}
 
 
 @typer_app.command()
-def command1():
-    print("Command 1")
+def create(username: str):
+    logger.debug("About to create a user")
+    logger.info(f"Deleting user: {username}")
+    logger.debug("Just created a user")
 
 
 @typer_app.command()
-def command2():
-    print("Command 2")
+def delete(username: str):
+    logger.debug("About to delete a user")
+    logger.info(f"Deleting user: {username}")
+    logger.debug("Just deleted a user")
 
 
-def main():
-    typer_app()
-
-
-if __name__ == "__main__":
-    main()
+@typer_app.callback()
+def main(verbose: int = typer.Option(0, "--verbose", "-v", count=True)):
+    if verbose == 0:
+        logging.basicConfig(level=logging.WARN)
+    elif verbose == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif verbose > 1:
+        logging.basicConfig(level=logging.DEBUG)
