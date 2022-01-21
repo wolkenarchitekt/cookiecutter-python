9a8111e7dffe1d87623a7d13f0df52bd97e7c845
Ingo Weinmann <mail@ingoweinmann.de>
2022-01-21 09:51:09 +0100

    Proper install_requires in setup.py

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -6,7 +6,9 @@ setup(
     packages=find_packages(exclude=("tests",)),
     version='0.0.1',
     install_requires=[
-        'typer',
+        'pydantic==1.8.2',
+        'python-dotenv==0.18.0',
+        'typer==0.3.2',
     ],
     entry_points={
         "console_scripts": ["{{cookiecutter.cli_script}} = {{cookiecutter.module_name}}.cli:typer_app"]
